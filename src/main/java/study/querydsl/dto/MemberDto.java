946a35bb69eead67fa3706fdf869e5724b6ac944
gunners <chakagae123@naver.com>
2024-05-22 11:27:45 +0900

    * 섹션 4. 프로젝션과 결과 반환 - @QueryProjection

--- src/main/java/study/querydsl/dto/MemberDto.java
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ public class MemberDto {
 //    public MemberDto() {
 //    }
 
+    @QueryProjection //Dto도 Q파일로 생성됨
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
